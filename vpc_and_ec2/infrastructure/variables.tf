b750ba207457957f0dbc6570e16bf4eb28a8b5d6
Dan Achin <dachin@c02w28g1htd6.corp.proofpoint.com>
2020-10-21 16:43:14 -0700

    added public and private subets

--- vpc_and_ec2/infrastructure/variables.tf
+++ vpc_and_ec2/infrastructure/variables.tf
@@ -7,3 +7,27 @@ variable "vpc_cidr" {
   default = "172.16.0.0/16"
   description = "VPC CIDR Block"
 }
+
+variable "public_subnet_1_cidr" {
+  description = "Public Subnet 1 cidr"
+}
+
+varibble "public_subnet_2_cidr" {
+  description = "Public Subnet 2 cidr"
+}
+
+variable "public_subnet_3_cidr" {
+  description = "Public Subnet 3 cidr"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "Private Subnet 1 cidr"
+}
+
+variable "private_subnet_2_cidr" {
+  description = "Private Subnet 2 cidr"
+}
+
+variable "private_subnet_3_cidr" {
+  description = "Private Subnet 3 cidr"
+}
