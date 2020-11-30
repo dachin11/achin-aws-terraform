bdabc94ba115cea851912399fbcd61ddf9e2ac55
Dan Achin <dachin@c02w28g1htd6.corp.proofpoint.com>
2020-11-30 11:31:27 -0800

    updated vpc subnets

--- vpc_and_ec2/infrastructure/vpc.tf
+++ vpc_and_ec2/infrastructure/vpc.tf
@@ -73,3 +73,47 @@ resource "aws_subnet" "private-subnet-3" {
     Name = "Private-Subnet-3"
   }
 }
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = "${aws_vpc.production-vpc.id}"
+  tags {
+    Name = "Public-Route-Table"
+  }
+}
+
+resource "aws_route" "private-route-table" {
+  vpc_id = "${aws_vpc.production-vpc.id}"
+  tags {
+    Name = "Private-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "public-subnet-1-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-1.id}"
+}
+
+resource "aws_route_table_association" "public-subnet-2-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-2.id}"
+}
+
+resource "aws_route_table_association" "public-subnet-3-association" {
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-3.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-1-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-1.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-2-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-2.id}"
+}
+
+resource "aws_route_table_association" "private-subnet-3-association" {
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-3.id}"
+}
