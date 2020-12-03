7a821acd56021f1dc2f77e5c5be258f25e7a5ff0
dachin11 <dachin@gmail.com>
2020-12-03 15:58:07 -0800

    needed to fix a bunch of code to back-rev to v11

--- vpc_and_ec2/infrastructure/variables.tf
+++ vpc_and_ec2/infrastructure/variables.tf
@@ -4,7 +4,7 @@ variable "region" {
 }
 
 variable "vpc_cidr" {
-  default = "172.16.0.0/16"
+  default = "10.0.0.0/16"
   description = "VPC CIDR Block"
 }
 
--- vpc_and_ec2/infrastructure/vpc.tf
+++ vpc_and_ec2/infrastructure/vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "var.region"
+  region = "${var.region}"
 }
 
 terraform {
@@ -16,7 +16,7 @@ resource "aws_vpc" "production-vpc" {
 }
 resource "aws_subnet" "public-subnet-1" {
   cidr_block        = "${var.public_subnet_1_cidr}"
-  vpc_id            = "aws_vpc.production-vpc.id"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
   availability_zone = "us-west-1a"
 
   tags = {
@@ -26,7 +26,7 @@ resource "aws_subnet" "public-subnet-1" {
 
 resource "aws_subnet" "public-subnet-2" {
   cidr_block        = "${var.public_subnet_2_cidr}"
-  vpc_id            = "aws_vpc.production-vpc.id"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
   availability_zone = "us-west-1b"
 
   tags = {
@@ -36,7 +36,7 @@ resource "aws_subnet" "public-subnet-2" {
 
 resource "aws_subnet" "public-subnet-3" {
   cidr_block        = "${var.public_subnet_3_cidr}"
-  vpc_id            = "aws_vpc.production-vpc.id"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
   availability_zone = "us-west-1c"
 
   tags = {
@@ -46,7 +46,7 @@ resource "aws_subnet" "public-subnet-3" {
 
 resource "aws_subnet" "private-subnet-1" {
   cidr_block        = "${var.private_subnet_1_cidr}"
-  vpc_id            = "aws_vpc.production-vpc.id"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
   availability_zone = "us-west-1a"
 
     tags = {
@@ -54,9 +54,9 @@ resource "aws_subnet" "private-subnet-1" {
     }
 }
 
-resource "aws_subnet" "private-subet-2" {
+resource "aws_subnet" "private-subnet-2" {
   cidr_block        = "${var.private_subnet_2_cidr}"
-  vpc_id            = "aws_vpc.production-vpc.id"
+  vpc_id            = "${aws_vpc.production-vpc.id}"
   availability_zone = "us-west-1b"
 
   tags = {
@@ -66,7 +66,7 @@ resource "aws_subnet" "private-subet-2" {
 
 resource "aws_subnet" "private-subnet-3" {
   cidr_block          = "${var.private_subnet_3_cidr}"
-  vpc_id              = "aws_vpc.production-vpc.id"
+  vpc_id              = "${aws_vpc.production-vpc.id}"
   availability_zone   = "us-west-1c"
 
   tags = {
@@ -89,38 +89,38 @@ resource "aws_route_table" "private-route-table" {
 }
 
 resource "aws_route_table_association" "public-subnet-1-association" {
-  route_table_id = "aws_route_table.public-route-table.id"
-  subnet_id      = "aws_subnet.public-subnet-1.id"
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-1.id}"
 }
 
 resource "aws_route_table_association" "public-subnet-2-association" {
-  route_table_id = "aws_route_table.public-route-table.id"
-  subnet_id      = "aws_subnet.public-subnet-2.id"
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-2.id}"
 }
 
 resource "aws_route_table_association" "public-subnet-3-association" {
-  route_table_id = "aws_route_table.public-route-table.id"
-  subnet_id      = "aws_subnet.public-subnet-3.id"
+  route_table_id = "${aws_route_table.public-route-table.id}"
+  subnet_id      = "${aws_subnet.public-subnet-3.id}"
 }
 
 resource "aws_route_table_association" "private-subnet-1-association" {
-  route_table_id = "aws_route_table.private-route-table.id"
-  subnet_id      = "aws_subnet.private-subnet-1.id"
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-1.id}"
 }
 
 resource "aws_route_table_association" "private-subnet-2-association" {
-  route_table_id = "aws_route_table.private-route-table.id"
-  subnet_id      = "aws_subnet.private-subnet-2.id"
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-2.id}"
 }
 
 resource "aws_route_table_association" "private-subnet-3-association" {
-  route_table_id = "aws_route_table.private-route-table.id"
-  subnet_id      = "aws_subnet.private-subnet-3.id"
+  route_table_id = "${aws_route_table.private-route-table.id}"
+  subnet_id      = "${aws_subnet.private-subnet-3.id}"
 }
 
-resource "aws_eip" "elastic-ip-for-nat-gateway" {
+resource "aws_eip" "elastic-ip-for-nat-gw" {
   vpc                       = true
-  associate_with_private_ip = "172.31.0.99"
+  associate_with_private_ip = "10.0.0.5"
 
   tags = {
     Name = "Production-EIP"
@@ -128,25 +128,25 @@ resource "aws_eip" "elastic-ip-for-nat-gateway" {
 }
 
 resource "aws_nat_gateway" "nat-gw" {
-  allocation_id = "${aws_eip.elastic_ip-for-nat-gw.id}"
-  subnet_id     = "aws_subnet.public-subnet-1.id"
+  allocation_id = "${aws_eip.elastic-ip-for-nat-gw.id}"
+  subnet_id     = "${aws_subnet.public-subnet-1.id}"
 
   tags = {
     Name = "Production-NAT-GW"
   }
 
-  depends_on = [aws_eip.elastic_ip-for-nat-gw]
+  depends_on = ["aws_eip.elastic-ip-for-nat-gw"]
 }
 
 resource "aws_route" "nat-gw-route" {
-  route_table_id         = "aws_route_table.private-route_table.id"
-  nat_gateway_id         = "aws_nat_gateway.nat-gw.id"
+  route_table_id         = "${aws_route_table.private-route-table.id}"
+  nat_gateway_id         = "${aws_nat_gateway.nat-gw.id}"
   destination_cidr_block = "0.0.0.0/0"
 
 }
 
 resource "aws_internet_gateway" "production-igw" {
-  vpc_id = "aws_vpc.production-vpc.id"
+  vpc_id = "{$aws_vpc.production-vpc.id}"
 
   tags = {
     Name = "Production-IGW"
@@ -154,7 +154,7 @@ resource "aws_internet_gateway" "production-igw" {
 }
 
 resource "aws_route" "public-internet-gw-route" {
-  route_table_id         = "aws_route_table.public-route-table.id"
-  gateway_id             = "aws_internet_gateway.production-igw.id"
+  route_table_id         = "${aws_route_table.public-route-table.id}"
+  gateway_id             = "${aws_internet_gateway.production-igw.id}"
   destination_cidr_block = "0.0.0.0/0"
 }
