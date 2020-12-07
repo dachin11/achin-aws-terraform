ece6c173bf391d1ea4773e456451b050a6395479
dachin11 <dachin@gmail.com>
2020-12-07 14:29:09 -0800

    added security groups to instances.tf

--- vpc_and_ec2/instances/instances.tf
+++ vpc_and_ec2/instances/instances.tf
@@ -15,3 +15,79 @@ data "terraform_remote_state" "network_configuration" {
     region = "${var.region}"
   }
 }
+
+resource "aws_security_group" "ec2_public_security_group" {
+  name        = "EC2-Public-SG"
+  description = "Inbound access from the internet to EC2 Instances"
+  vpc_id      = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  ingress {
+   from_port   = 80
+   protocol    = "TCP"
+   to_port     = 80
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    protocol = "TCP"
+    to_port = 22
+    cidr_blocks = ["208.84.66.30/32"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_private_security_group" {
+  name = "EC2-Private-SG"
+  description = "Allows public security group resources access our private SG"
+  vpc_id = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["${aws_security_group.ec2_public_security_group}"]
+  }
+
+  ingress {
+    from_port = 80
+    protocol = "TCP"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow healthchecking for instances using this security group"
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "elb_security_group" {
+  name = "ELB-SG"
+  description = "Elastic LB Security Group"
+  vpc_id = "${data.terraform_remote_state.network_configuration.vpc_id}"
+
+  ingress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all traffic to LB"
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
