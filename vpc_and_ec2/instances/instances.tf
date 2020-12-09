a7261e1fe747a656bcaadaf33348fd0dee52085a
dachin11 <dachin@gmail.com>
2020-12-09 15:25:57 -0800

    added ami

--- vpc_and_ec2/instances/instances.tf
+++ vpc_and_ec2/instances/instances.tf
@@ -138,3 +138,13 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "EC2-IAM-Instance-Profile"
   role = aws_iam_role.ec2_iam_role.name
 }
+
+data "aws_ami" "launch_configuration_ami" {
+  most_recent = true
+  owners = ["self"]
+
+  filter {
+    name = "owner-alias"
+    values = ["amazon"]
+  }
+}
