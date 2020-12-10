3b0c6872d39252c6f6e662c222e07b116bf058b6
dachin11 <dachin@gmail.com>
2020-12-10 12:59:28 -0800

    updated launch configuration info and added variables

--- vpc_and_ec2/instances/variables.tf
+++ vpc_and_ec2/instances/variables.tf
@@ -10,3 +10,12 @@ variable "remote_state_bucket" {
 variable "remote_state_key" {
   description = "Key name for layer 1 remote state"
 }
+
+variable "ec2_instance_type" {
+  description = "EC2 Instance type to launch"
+}
+
+variable "key_pair_name" {
+  default ="dachin_aws"
+  description = "Keypair to use to connect to EC2 instances"
+}
