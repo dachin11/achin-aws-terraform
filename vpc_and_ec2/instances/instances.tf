d7da90ef298d97ea34f856f7a99113696a2e052c
dachin11 <dachin@gmail.com>
2020-12-10 15:57:45 -0800

    added elb

--- vpc_and_ec2/instances/instances.tf
+++ vpc_and_ec2/instances/instances.tf
@@ -167,3 +167,47 @@ resource "aws_launch_configuration" "ec2_private_launch_configuration" {
     echo "<html><body><h1>Hello from Production Backend at instance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
   EOF
 }
+
+resource "aws_launch_configuration" "ec2_public_launch_configuration" {
+  image_id                    = data.aws_ami.launch_configuration_ami.id
+  instance_type               = var.ec2_instance_type
+  key_name                    = var.key_pair_name
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.ec2_instance_profile.name
+  security_groups             = [aws_security_group.ec2_public_security_group.id]
+
+  user_data = <<EOF
+    #!/bin/bash
+    yum update -y
+    yum install httpd24 -y
+    service httpd start
+    chkconfig httpd on
+    export INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+    echo "<html><body><h1>Hello from Production Web App at instance <b>"$INSTANCE_ID"</b></h1></body></html>" > /var/www/html/index.html
+  EOF
+}
+
+resource "aws_elb" "webapp_load_balancer" {
+  name            = "Production-WebApp-LoadBalancer"
+  internal        = false
+  security_groups = [aws_security_group.elb_security_group.id]
+  subnets         = [
+    data.terraform_remote_state.network_configuration.public_subnet_1_id,
+    data.terraform_remote_state.network_configuration.public_subnet_2_id,
+    data.terraform_remote_state.network_configuration.public_subnet_3_id
+  ]
+  listener {
+    instance_port = 80
+    instance_protocol = "HTTP"
+    lb_port = 80
+    lb_protocol = "HTTP"
+  }
+
+  health_check {
+    healthy_threshold = 5
+    interval = 30
+    target = "HTTP:80/index.html"
+    timeout = 10
+    unhealthy_threshold = 5
+  }
+}
