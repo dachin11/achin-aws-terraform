b243d77989136bf0d24aaa923d20bfcea250b8cf
dachin11 <dachin@gmail.com>
2020-12-11 15:58:26 -0800

    added autoscaling alerts

--- vpc_and_ec2/instances/instances.tf
+++ vpc_and_ec2/instances/instances.tf
@@ -330,4 +330,12 @@ resource "aws_sns_topic_subscription" "webapp_production_autoscaling_sms_subscri
   topic_arn = aws_sns_topic.webapp_production_autoscaling_alert_topic.arn
 }
 
-
+resource "aws_autoscaling_notification" "webapp_autoscaling_notification" {
+  group_names = [aws_autoscaling_group.ec2_public_autoscaling_group]
+  notifications = [
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+    "autoscaling:EC2_INSTANCE_TERMINATE",
+    "autoscaling:EC2_INSTANCE_LAUNCH_ERROR"
+  ]
+  topic_arn = aws_sns_topic.webapp_production_autoscaling_alert_topic.arn
+}
